cee00b9de61eaa1c09e50d2541eb896eca07ee00
Priyanshi <priyanshiray195@gmail.com>
2024-01-05 13:43:07 +0530

    Submitted by Priyanshi Ray

--- program1.js
+++ program1.js
@@ -3,7 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const openingBrackets = ['(', '[', '{'];
+    const closingBrackets = [')', ']', '}'];
+
+    for (let i = 0; i < s.length; i++) {
+        const currentChar = s[i];
+
+        if (openingBrackets.includes(currentChar)) {
+            stack.push(currentChar);
+        } else if (closingBrackets.includes(currentChar)) {
+            const lastOpeningBracket = stack.pop();
+
+            if (!lastOpeningBracket || openingBrackets.indexOf(lastOpeningBracket) !== closingBrackets.indexOf(currentChar)) {
+                return false; 
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentSymbol = s[i];
+        const nextSymbol = s[i + 1];
+
+        if (romanValues[currentSymbol] < romanValues[nextSymbol]) {
+            result -= romanValues[currentSymbol];
+        } else {
+            result += romanValues[currentSymbol];
+        }
+    }
+
+    return result;
 };
 
 
